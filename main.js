0ecdf8d2b053453ed1816fc326d2622ed9566c13
piedcipher <pateltirth454@gmail.com>
2022-03-15 01:09:20 +0530

    add options page for block-list, add bootstrap for design

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-document.getElementById("clear-all").onclick = (e) => {
-    clearBlockList();
-};
-
-// clear all the urls in the block-list
-clearBlockList = () => {
-    chrome.storage.sync.get(null, (blockList) => {
-        Object.keys(blockList).forEach(e => removeUrl(e));
-    });
-};
-
-// removes the url in the block-list
-removeUrl = (url) => {
-    chrome.storage.sync.remove(url.toString(), () => {
-        displayBlockList();
-    });
-};
-
 // stores the url in the block-list
 storeUrl = (url) => {
     if (!url || url.trim().length === 0 || url === "https://i.imgur.com/uNuKDAZ.jpg" || !url.startsWith("https://")) {
@@ -24,7 +6,6 @@ storeUrl = (url) => {
     chrome.storage.sync.set({
         [url]: url
     }, () => {
-        displayBlockList();
     });  
 };
 
@@ -40,29 +21,11 @@ document.getElementById("add-to-blocklist").onclick = (e) => {
     getCurrentTabThenExecute(storeUrl);
 }
 
-// renders block-list on popup (UI)
-displayBlockList = () => {
-    // clears the block-list
-    const blockList = document.getElementById("block-list");
-    blockList.innerHTML = '';
-
-    // get all block-listed urls from this extension's storage
-    chrome.storage.sync.get(null, (items) => {
-        var allKeys = Object.keys(items);
-
-        // display all block-listed urls in UI
-        allKeys.forEach(e => {
-            blockList.innerHTML += `<li><a target="_blank" href=${e}>${e}</a><br><br><button class="remove-button" value="${e}">Remove</button></li><br><br><hr><br><br>`;
-        });
-        
-        const removeButtonTags = blockList.getElementsByClassName("remove-button");
-        for (let i = 0; i < removeButtonTags.length; i++) {
-            // adds onclick event to remove that url from block-list
-            removeButtonTags[i].onclick = (f) => {
-                removeUrl(removeButtonTags[i].value);
-            };
-        }
-    });
-};
-
-displayBlockList();
+// opens block-list
+document.querySelector('#go-to-options').addEventListener('click', function() {
+    if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage();
+    } else {
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+});
---
+++ options.js
@@ -0,0 +1,49 @@
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    // for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    //     console.log(
+    //     `Storage key "${key}" in namespace "${namespace}" changed.`,
+    //     `Old value was "${oldValue}", new value is "${newValue}".`
+    //     );
+    // }
+    displayBlockList();
+});
+
+// clear all the urls in the block-list
+document.getElementById("clear-all").onclick = (e) => {
+    chrome.storage.sync.get(null, (blockList) => {
+        Object.keys(blockList).forEach(e => removeUrl(e));
+    });
+};
+
+// removes the url in the block-list
+removeUrl = (url) => {
+    chrome.storage.sync.remove(url.toString(), () => {
+    });
+};
+
+// renders block-list on popup (UI)
+displayBlockList = () => {
+    // clears the block-list
+    const blockList = document.getElementById("block-list");
+    blockList.innerHTML = '';
+
+    // get all block-listed urls from this extension's storage
+    chrome.storage.sync.get(null, (items) => {
+        var allKeys = Object.keys(items);
+
+        // display all block-listed urls in UI
+        allKeys.forEach(e => {
+            blockList.innerHTML += `<button class="remove-button" value="${e}">Remove</button>&emsp;<a target="_blank" href=${e}>${e}</a><br><br>`;
+        });
+        
+        const removeButtonTags = blockList.getElementsByClassName("remove-button");
+        for (let i = 0; i < removeButtonTags.length; i++) {
+            // adds onclick event to remove that url from block-list
+            removeButtonTags[i].onclick = (f) => {
+                removeUrl(removeButtonTags[i].value);
+            };
+        }
+    });
+};
+
+displayBlockList();
