ef7aa63f822d054d489275ac4a25e58bfa6036bd
piedcipher <pateltirth454@gmail.com>
2022-05-08 02:44:54 +0530

    :sailboat: add support for custom images, preparing for v0.0.2

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 // opens the meme if you're on block-listed site
 chrome.storage.sync.get(null, (blockList) => {
     if (Object.keys(blockList).includes(window.location.origin)) {
-        window.location = "https://i.imgur.com/uNuKDAZ.jpg";
+        chrome.storage.local.get(["block-image"], (result) => {
+            document.getElementsByTagName("body")[0].innerHTML = `<div style="display: flex; align-items: center; justify-content: center; height: 100%;"><img src=${result["block-image"] ?? "https://i.imgur.com/uNuKDAZ.jpg"}></div>`;
+        });
     }
 });
--- options.js
+++ options.js
@@ -6,7 +6,7 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
 // clear all the urls in the block-list
 document.querySelector("#clear-all").onclick = (e) => {
     chrome.storage.sync.get(null, (blockList) => {
-        Object.keys(blockList).forEach(e => removeUrl(e));
+        Object.keys(blockList).forEach(e => e !== "block-image" && removeUrl(e));
     });
 };
 
@@ -24,7 +24,7 @@ displayBlockList = () => {
     // get all block-listed urls from this extension's storage
     chrome.storage.sync.get(null, (items) => {
         var allKeys = Object.keys(items);
-
+        delete allKeys["block-image"];
         // display all block-listed urls in UI
         allKeys.forEach(e => {
             blockList.innerHTML += `<button class="remove-button" value="${e}">Remove</button>&emsp;<a target="_blank" href=${e}>${e}</a><br><br>`;
@@ -38,7 +38,40 @@ displayBlockList = () => {
             };
         }
     });
+
+    // renders current block image
+    chrome.storage.local.get(["block-image"], (result) => {
+        document.querySelector("#block-image-view").src = result["block-image"] ?? "https://i.imgur.com/uNuKDAZ.jpg";
+    });
 };
 
 // initial render
 displayBlockList();
+
+// handles custom image input from the user
+document.querySelector("#block-image").onchange = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = (_) => {
+        document.querySelector("#block-image-view").src = reader.result;
+        if (e.target.files[0].size <= 1000000 &&
+            (e.target.files[0].type === "image/jpeg"
+            || e.target.files[0].type === "image/png")) {
+            document.querySelector("#save-block-image").removeAttribute("disabled");
+        } else {
+            document.querySelector("#save-block-image").disabled = "true";
+        }
+    };
+};
+
+// saves custom (block) image to chrome's local storage (not localStorage)
+document.querySelector("#save-block-image").onclick = (e) => {
+    chrome.storage.local.set({
+        "block-image": document.querySelector("#block-image-view").src
+    }, () => {
+        document.querySelector("#save-block-image").disabled = "true";
+        chrome.storage.local.get(["block-image"], (result) => {
+            document.querySelector("#block-image-view").src = result["block-image"];
+        });
+    });
+};
